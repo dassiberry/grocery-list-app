24ae4f4d613b016a3bd52d5979256597cd54cf7b
Dassi Berry <dassiberry@gmail.com>
2026-04-26 18:09:35 -0400

    initial commit with barebone code

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div className={styles.container}>
+      <h1>My Grocery List</h1>
+      <p>Start adding items to your list!</p>
+    </div>
+  );
+}
+
+export default App
